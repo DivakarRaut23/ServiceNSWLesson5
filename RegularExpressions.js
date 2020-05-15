44de5339c5aeccb4fd77f537e454a1ce49a49d88
Divakar Raut <divakar.raut@service.nsw.gov.au>
2020-05-15 21:59:54 +1000

    Lesson 5 final

---
+++ RegularExpressions.js
@@ -0,0 +1,18 @@
+const EMAIL_REGEX = /(?:[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/i;
+const URL_REGEX = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/i;
+
+// Write a function that determines if a string contains a valid email address
+
+function testValidEmail(testString){
+    return EMAIL_REGEX.test(testString);
+}
+//console.log(testValidEmail("This is my fight with Javascript and i am determined to conquer it. You can reach me on divakar.raut@service.nsw.gov.au "));
+
+
+// Write a function to find the index of the first character of a valid URL in a given string
+
+ //String.search(regEx)
+function findIndexOfValidURL(testString) {
+    return testString.search(URL_REGEX);
+}
+console.log(findIndexOfValidURL("Visit us on:  https://www.manutd.com/enu/ to explore more"));
